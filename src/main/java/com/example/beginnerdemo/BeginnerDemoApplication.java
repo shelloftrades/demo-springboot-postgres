f44444d06d149133d0a0c69a619cad606a647510
Catapan <smcatapan@gmail.com>
2021-06-17 22:22:36 +0800

    finished 1 complete cycle of basic CRUD API <-> POSTGRES

--- src/main/java/com/example/beginnerdemo/BeginnerDemoApplication.java
+++ src/main/java/com/example/beginnerdemo/BeginnerDemoApplication.java
@@ -3,11 +3,13 @@ package com.example.beginnerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class BeginnerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeginnerDemoApplication.class, args);
-	}
+}
 
 }
--- src/main/resources/db/migration/V1__PersonTable.sql
+++ src/main/resources/db/migration/V1__PersonTable.sql
@@ -2,3 +2,10 @@ CREATE TABLE person (
     id UUID NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+INSERT INTO person VALUES (uuid_generate_v4(), 'James Pique');
+INSERT INTO person VALUES (uuid_generate_v4(), 'James Bond');
+INSERT INTO person VALUES (uuid_generate_v4(), 'Christen Press');
+INSERT INTO person VALUES (uuid_generate_v4(), 'Alex Morgan');
+INSERT INTO person VALUES (uuid_generate_v4(), 'Lionel Messi');
+INSERT INTO person VALUES (uuid_generate_v4(), 'Christiano Ronaldo');
