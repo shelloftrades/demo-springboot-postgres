103025eba3ae4254b57f9b083961be2dfab8ed2e
Catapan <smcatapan@gmail.com>
2021-06-19 00:38:39 +0800

    test

--- src/main/java/com/example/beginnerdemo/BeginnerDemoApplication.java
+++ src/main/java/com/example/beginnerdemo/BeginnerDemoApplication.java
@@ -3,13 +3,11 @@ package com.example.beginnerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
-
 @SpringBootApplication
 public class BeginnerDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeginnerDemoApplication.class, args);
-}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BeginnerDemoApplication.class, args);
+        
+    }
 }
