f76285595af761091133d6d68be5fa979c9c1f04
Catapan <smcatapan@gmail.com>
2021-07-06 21:16:38 +0800

    added Security Configurer:ongoing

--- src/main/java/com/shelloftrades/beginnerdemo/security/SecurityConfigurer.java
+++ src/main/java/com/shelloftrades/beginnerdemo/security/SecurityConfigurer.java
@@ -1,6 +1,27 @@
 package com.shelloftrades.beginnerdemo.security;
 
-import java.lang.annotation.Annotation;
+import com.shelloftrades.beginnerdemo.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class SecurityConfigurer {
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(myUserDetailService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
